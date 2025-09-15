c3ceeed02984b1d3e977c8a7257303f7793f9934
emshelburne <158862064+emshelburne@users.noreply.github.com>
2025-09-14 23:00:46 -0700

    trying to deploy again

--- DEMO/_boot_min.py
+++ DEMO/_boot_min.py
@@ -1,3 +1,6 @@
+import os
+os.environ["STREAMLIT_BROWSER_GATHER_USAGE_STATS"] = "false"
+
 import streamlit as st
 st.set_page_config(page_title="Boot OK", layout="centered")
 st.write(" Minimal app booted")
